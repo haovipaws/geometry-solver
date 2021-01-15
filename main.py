8c3df09cccee4140254edd6e5880066fe19011c3
Haovipaws <haovi@tuta.io>
2021-01-15 12:35:36 -0800

    fixed circular import

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from sys import argv
+import math
 
 answer = []
 
@@ -9,13 +10,16 @@ if argv[1] == "prism":
     answer.append(float(input("[prism mode]: what is the base length ")))
     answer.append(float(input("[prism mode]: what is the height ")))
 
-    print('[prism mode]: the volume is: ', answer[0]*answer[1]*answer[2])
-    print('[prism mode]: the surface area is: ', answer[0]*answer[1]+answer[1]*answer[2]+answer[2]*answer[0])
+    volume = answer[0]*answer[1]*answer[2]
+    area = answer[0]*answer[1]+answer[1]*answer[2]+answer[2]*answer[0]
+    print(f'[prism mode]: the volume is: {volume}')
+    print(f'[prism mode]: the surface area is: {area}')
 
 elif argv[1] == "cylinder":
     answer.append(float(input("[cylinder mode]: what is the radius ")))
     answer.append(float(input("[cylinder mode]: what is the height ")))
 
+    volume = (answer[0]*math.pi**2)*answer[1]
     print('[cylinder mode]: the volume is: ')
     print('[cylindar mode]: the surface area is: ')
 
@@ -24,7 +28,7 @@ elif argv[1] == "pyramid":
     answer.append(float(input("[pyramid mode]: what is the height ")))
 
     print('[pyramid mode]: the volume is: ', (answer[0]*answer[0]*answer[1])/3)
-    print('[pyramid mode]: the surface area is: ', answer[0]*answer[0]+answer[0]*answer[1]+answer[1]*answer[0])
+    print('[pyramid mode]: the surface area is: ')
 
 elif argv[1] == "sphere":
     answer.append(float(input("[sphere mode]: what is the radius")))
