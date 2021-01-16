f86028f2d5829a73c0ce609c9864c3b6d97e1f49
Haovipaws <haovi@tuta.io>
2021-01-15 17:42:16 -0800

    add calculations for cylinder

--- main.py
+++ main.py
@@ -11,27 +11,28 @@ if argv[1] == "prism":
     answer.append(float(input("[prism mode]: what is the height ")))
 
     volume = answer[0]*answer[1]*answer[2]
-    area = answer[0]*answer[1]+answer[1]*answer[2]+answer[2]*answer[0]
+    surface_area = answer[0]*answer[1]+answer[1]*answer[2]+answer[2]*answer[0]
     print(f'[prism mode]: the volume is: {volume}')
     print(f'[prism mode]: the surface area is: {area}')
 
 elif argv[1] == "cylinder":
-    answer.append(float(input("[cylinder mode]: what is the radius ")))
-    answer.append(float(input("[cylinder mode]: what is the height ")))
+    radius = float(input("[cylinder mode]: what is the radius "))
+    height = float(input("[cylinder mode]: what is the height "))
 
-    volume = (answer[0]*math.pi**2)*answer[1]
-    print('[cylinder mode]: the volume is: ')
-    print('[cylindar mode]: the surface area is: ')
+    volume = (radius*math.pi**2)*height
+    surface_area = 2*radius*math.pi**2+2*math.pi*radius*height
+    print(f'[cylinder mode]: the volume is: {volume}')
+    print(f'[cylindar mode]: the surface area is: {area}')
 
 elif argv[1] == "pyramid":
-    answer.append(float(input("[pyramid mode]: what is the base width ")))
-    answer.append(float(input("[pyramid mode]: what is the height ")))
+    width = float(input("[pyramid mode]: what is the base width "))
+    height = float(input("[pyramid mode]: what is the height "))
 
     print('[pyramid mode]: the volume is: ', (answer[0]*answer[0]*answer[1])/3)
     print('[pyramid mode]: the surface area is: ')
 
 elif argv[1] == "sphere":
-    answer.append(float(input("[sphere mode]: what is the radius")))
+    radius = float(input("[sphere mode]: what is the radius"))
 
     print('[sphere mode]: the volume is: ')
     print('[sphere mode]: the surface area is: ')
