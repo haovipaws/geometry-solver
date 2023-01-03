d063772ae2c4ddbea52b6008d19f9cae3a9f92c2
haovipaws <32804660+haovipaws@users.noreply.github.com>
2023-01-03 14:43:05 -0800

    organize variables and clean up code

--- main.py
+++ main.py
@@ -5,34 +5,51 @@ import math
 
 answer = []
 
-if argv[1] == "prism":
-    answer.append(float(input("[prism mode]: what is the base width ")))
-    answer.append(float(input("[prism mode]: what is the base length ")))
-    answer.append(float(input("[prism mode]: what is the height ")))
+volume = "unknown"
+surface_area = "unknown"
 
-    volume = answer[0]*answer[1]*answer[2]
-    surface_area = answer[0]*answer[1]+answer[1]*answer[2]+answer[2]*answer[0]
+if argv[1] == "prism":
+    width = float(input("[prism mode]: what is the base width "))
+    length = float(input("[prism mode]: what is the base length "))
+    height = float(input("[prism mode]: what is the height "))
+
+    # Calculations
+    volume = width * length * height
+    surface_area = width * length + length * height + height * width
+    
+    # Display answer
     print(f'[prism mode]: the volume is: {volume}')
-    print(f'[prism mode]: the surface area is: {area}')
+    print(f'[prism mode]: the surface area is: {surface_area}')
 
 elif argv[1] == "cylinder":
     radius = float(input("[cylinder mode]: what is the radius "))
     height = float(input("[cylinder mode]: what is the height "))
 
-    volume = (radius*math.pi**2)*height
-    surface_area = 2*radius*math.pi**2+2*math.pi*radius*height
+    volume = (radius * math.pi ** 2) * height
+    surface_area = 2 * radius * math.pi ** 2 + 2 * math.pi * radius * height
+    
+    # Display answer
     print(f'[cylinder mode]: the volume is: {volume}')
-    print(f'[cylindar mode]: the surface area is: {area}')
+    print(f'[cylindar mode]: the surface area is: {surface_area}')
 
 elif argv[1] == "pyramid":
     width = float(input("[pyramid mode]: what is the base width "))
     height = float(input("[pyramid mode]: what is the height "))
 
-    print('[pyramid mode]: the volume is: ', (answer[0]*answer[0]*answer[1])/3)
-    print('[pyramid mode]: the surface area is: ')
+    # Calculations
+    volume = (width * width * height) / 3
+    # TODO surface area
+
+    # Display answer
+    print(f'[pyramid mode]: the volume is: {volume}')
+    print(f'[pyramid mode]: the surface area is: {surface_area}')
 
 elif argv[1] == "sphere":
     radius = float(input("[sphere mode]: what is the radius"))
 
-    print('[sphere mode]: the volume is: ')
-    print('[sphere mode]: the surface area is: ')
+    # Calculations
+    # TODO: add calculations or pretend to
+
+    # Display answer
+    print(f'[sphere mode]: the volume is: {volume}')
+    print(f'[sphere mode]: the surface area is: {surface_area}')
